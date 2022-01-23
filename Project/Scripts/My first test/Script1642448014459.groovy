a7aac92db5fd1e07b372aa44a1cd05d1988ef39d
unknown <zhukovflycode@gmail.com>
2022-01-23 20:15:03 +0300

    Основная база

---
+++ Project/Scripts/My first test/Script1642448014459.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.google.com/')
+
+WebUI.maximizeWindow()
+
+WebUI.setText(findTestObject('Object Repository/input Fly code'), 'fly code')
+
+WebUI.sendKeys(findTestObject('Object Repository/input Fly code'), Keys.chord(Keys.ENTER))
+
+WebUI.click(findTestObject('Object Repository/Press Fly Code'))
+
+WebUI.verifyElementText(findTestObject('Object Repository/Number'), '+7 499 112 34 72')
+
+WebUI.verifyElementPresent(findTestObject('Page_Digital-/Logo'), 1)
+
+WebUI.verifyElementPresent(findTestObject('Page_Digital-/Create'), 1)
+
+WebUI.navigateToUrl('https://fly-code.com/')
+
