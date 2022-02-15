55fd914a2930db711b7d9d90fb49825594588a42
Zhukov-Pavel <Zhukovflycode@gmail.com>
2022-02-15 15:05:27 +0300

    Удаление Testing- не нужен

--- Testing/Object Repository/Logo.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>Logo</name>
-   <tag></tag>
-   <elementGuidId>ea3090cc-8d34-413c-b05f-28cdf677889e</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
